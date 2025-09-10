a8ca0c95b9a3a4c6807c2392a7930725f3ed59ac
Enestacy <muzotcka@mail.ru>
2025-09-10 05:06:08 +0300

    chore: remove extra envs configuration and related references

--- src/config/svc.config.ts
+++ src/config/svc.config.ts
@@ -4,7 +4,6 @@ import { AppConfig } from './types';
 export default registerAs(
   'service',
   (): AppConfig => ({
-    appPort: process.env.APP_PORT || '3000',
     appEnv: process.env.NODE_ENV || 'development',
   }),
 );
--- src/config/types/svc-config.type.ts
+++ src/config/types/svc-config.type.ts
@@ -1,4 +1,3 @@
 export type AppConfig = {
-  appPort: string;
   appEnv: string;
 };
